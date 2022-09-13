4f6f300436e4d4b22084c1514b59aa2e8d33a2a5
Evie <evangeliapal@gmail.com>
2022-09-13 12:15:04 +0100

    Test that the beer controller request for specific beer id was successful

--- test/controllers/beers_controller_test.rb
+++ test/controllers/beers_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class BeersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should respond with info about beer id" do
+    get "/beers/2"
+    assert_response :success
+  end
 end
