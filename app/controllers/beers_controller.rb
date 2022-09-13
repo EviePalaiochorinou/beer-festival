f40da4c1575d7c7c123d5803e0ac197f25c4cd05
Evie <evangeliapal@gmail.com>
2022-09-13 13:27:43 +0100

    Refactor controller get_beers method and private conversion method to take optional parameter for beer name

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,6 @@
 require_relative "../clients/punk_api_client"
 class BeersController < ApplicationController
-  def initialize()
+	def initialize()
 		@beer_client = PunkAPIClient.new
 	end
 
@@ -16,14 +16,13 @@ class BeersController < ApplicationController
 				return render json: {'message': 'Shop is closed'}, status: :internal_server_error
 			end
 		end
-
 		beer_object = convert_beer(beers[0])
 		render json: beer_object
 	end
 
-  def get_beers
+	def get_beers
 		begin
-			beers = @beer_client.get_beers
+			beers = @beer_client.get_beers(params[:name])
 		rescue Exception => e
 			if e.message == "Beers unreachable"
 				return render json: {'message': 'Cannot serve you beers right now'}, status: :service_unavailable
@@ -32,21 +31,24 @@ class BeersController < ApplicationController
 			end
 		end
 
-		new_response = convert_beers(beers)
+		new_response = convert_beers(beers, params[:name])
 		render json: new_response
 	end
 
   private
-  def convert_beers(raw_beers)
+	def convert_beers(raw_beers, name)
 		converted_beers = []
 		raw_beers.each do |beer|
+			if !name.nil? && name.downcase != beer[:name].downcase
+				next
+			end
 			converted_beer = convert_beer(beer)
 			converted_beers.push(converted_beer)
 		end
 		return converted_beers
 	end
 
-  def convert_beer(beer)
+	def convert_beer(beer)
 		converted_beer = {
 			"identifier": beer[:id],
 			"name": beer[:name],
