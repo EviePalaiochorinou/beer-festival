471dfffc6d5dfc55d6950e5d164d5c3b2fbffb9e
Evie <evangeliapal@gmail.com>
2022-09-13 12:35:23 +0100

    Test the controller method to get all beers

--- test/controllers/beers_controller_test.rb
+++ test/controllers/beers_controller_test.rb
@@ -1,10 +1,19 @@
-require "test_helper"
-
+require_relative "../test_helper"
+require_relative "../../spec/spec_helper"
+require_relative "../../spec/rails_helper"
 class BeersControllerTest < ActionDispatch::IntegrationTest
-  
+
   # Was the web request successful?
   test "should respond with info about beer id" do
     get "/beers/2"
     assert_response :success
+  end
+
+  # Was the correct response given?
+  test "should get all beers" do
+    get "/beers"
+    print(response.body)
+    assert_response :success
+    expect(JSON.parse(response.body)).to equal()
   end
 end
